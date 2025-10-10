4c1f51fed39221b151c370e223c5df4d05d56394
Catalin Popa <catalin@192.168.1.134>
2025-10-11 01:53:41 +0300

    first homework

---
+++ tema1/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+
+//Prima problema
+void inlocuirePare(int* v, int n) {
+    for (int i = 0;i<n;i++) {
+        if (v[i]%2==0)
+            v[i] = 0;
+    }
+}
+//A doua problema
+int* dublarePare(int* v, int n, int *cntPare) {
+    (*cntPare) = n;
+    for (int i = 0;i<n;i++) {
+        if (v[i]%2==0)
+            (*cntPare)++;
+    }
+    int *aux = (int*)malloc((*cntPare)*sizeof(int));
+    int j = 0;
+    for (int i = 0;i<n;i++) {
+        aux[j++]= v[i];
+        if (v[i]%2==0)
+            aux[j++] = v[i];
+    }
+    return aux;
+}
+// A treia problema
+void deletePare(int*v, int *n) {
+    int j = 0;
+    for (int i = 0;i<(*n);i++) {
+        if (v[i]%2!=0)
+        v[j++] = v[i];
+    }
+    (*n)=j;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int* v = (int*)malloc(n*sizeof(int));
+    for (int i = 0;i<n;i++) {
+        cin >> v[i];
+    }
+
+    //De comentat pentru celelalte functi
+    // inlocuirePare(v,n);
+    // for (int i = 0;i<n;i++) {
+    //     cout << v[i] << ' ';
+    // }
+
+    cout << "\n\n\n";
+    int cntPare;
+    int* dubluPare = dublarePare(v,n,&cntPare);
+    for (int i = 0;i<cntPare;i++) {
+        cout << dubluPare[i] << ' ';
+    }
+    // cout << "\n\n\n";
+    // deletePare(v,&n);
+    // for (int i = 0;i<n;i++)
+    //     cout << v[i] << ' ';
+
+    free(dubluPare);
+    free(v);
+}
