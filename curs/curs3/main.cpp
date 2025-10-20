8290c18b201064f9aa0544a943dab0e860e5ae0a
Catalin Popa <catalin@Catalins-MacBook-Pro.local>
2025-10-20 10:24:10 +0300

    curs3.1 aproape gata

--- curs/curs3/main.cpp
+++ curs/curs3/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class salariat {
     int marca;
-    char nume[20];
+    char *nume;
     int ol;
     int os;
 
@@ -11,9 +11,10 @@ class salariat {
 
 public:
     //constructor implicit
-    salariat() : marca(0), ol(0), os(0)
+    salariat() :marca(genm), ol(0), os(0)
     {
         genm++;
+        nume = new char[6];
         strcpy(nume, "Zorro");
     }
 
@@ -21,35 +22,63 @@ public:
     salariat(const char* fnume, int fol, int fos) : marca(genm), ol(fol), os(fos)
     {
         genm++;
+        nume = new char[strlen(fnume) + 1];
         strcpy(nume, fnume);
     }
 
-    void Afisare()
+    // void afisare()
+    // {
+    //     cout << marca << " " << nume << " " << ol << " " << os << endl;
+    // }
+
+    friend ostream& operator<<(ostream& os,salariat& s) //se comporta ca o functie externa clasei,!!!! nu primeste this !!!!,
+    //dar primeste atributele din private
     {
-        cout << marca << " " << nume << " " << ol << " " << os << endl;
+        os << s.marca << " " << s.os << s.ol;
+        return os;
     }
 
     int get_marca() { return marca; }
 
-    static int Marca_urm_sal() { return genm;}
-
+    static int Marca_urmator_sal() { return genm; }
 
     ~salariat() { cout << "Destructor!!!" << endl; }
+
+    salariat& operator=(const salariat& obs)
+    {
+        delete[] nume;
+        nume = new char[strlen(obs.nume) + 1];
+        strcpy(nume, obs.nume);
+        ol = obs.ol;
+        os = obs.os;
+
+        return *this;
+    }
 };
 
 int salariat::genm = 100;
 
 int main()
 {
-    cout << "Marca urm. obiect: " << salariat::Marca_urm_sal() << endl;
-    salariat s1, s2("Mirel", 12, 15), * ps;
-    cout << "Marca urm. obiect: " << ps->Marca_urm_sal() << endl;
+    cout << salariat::Marca_urmator_sal()<<endl;
+    salariat s1, s2("Mirel", 12, 15),s3("Ion", 13,14), * ps;
+
+    //s1 = s2; profu zicea ca nu merge dar mie mi merge si fara sa suprascriu operatorul
+
     s1.Afisare();
     s2.Afisare();
     //aici s au terminat duratele de viata pt s1 si s2 deci vedem mesajul destructor
+    s1 = s2;
+
+    cout << "Dupa atribuire " << endl;
+    s1.Afisare();
+    s2.Afisare();
+    s3=s2=s1;
+    cout << "ceva ce facem" << endl;
+    s3.Afisare();
     ps = new salariat("Gigi", 50, 12.5);
-    cout << "Marca urm. obiect: " << salariat::Marca_urm_sal() << endl;
     ps->Afisare();
+    cout << salariat::Marca_urmator_sal()<<endl;
     //salariatul alocat dinamic trebuie dezalocat manual :(
     delete ps;
 
